fdd6761b41491c427071cb7c38e0804fe3ebbae6
aksh-git <ann0m@protonmail.com>
2022-01-15 19:15:13 +0530

    master

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-//window.onload = ()=>{
-//   
-//} 
-console.log("welcome to chess");
+//Variables
+let enterBtn = document.getElementById("enterBtn");
+let mainHead = document.getElementById("mainHead");
+let loader = document.getElementById("loader");
+
+window.onload = ()=>{
+   loader.style.display="none";
+} 
+
+//Handle click
+document.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      enterBtn.click();
+    }
+}); 
+
+//onpress enter
+function enterPressed(){
+    mainHead.style.display="none";
+}
